c24281158e7eab4c7e05dc1658f8911e9c38ee9c
Trixtar <magisterludi1789@gmail.com>
2019-07-06 20:55:34 +0200

    Initialize repository

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'next/link'
+import Head from '../components/head'
+import Nav from '../components/nav'
+
+const Home = () => (
+  <div>
+    <Head title="Available jobs" />
+    <Nav />
+  </div>
+)
+
+export default Home
